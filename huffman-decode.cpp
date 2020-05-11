80a6abee8b97eab3d982fc91bb798c9512a26a70
mtnshh <44230229+mtnshh@users.noreply.github.com>
2020-05-12 02:47:31 +0530

    decoding huffman-algos
    Input: 
    17
     98 ->     000
    110 ->     001
    104 ->    0100
    115 ->   01010
    100 ->   01011
    107 ->     011
    106 ->     100
    108 ->    1010
    114 ->    1011
     99 ->  110000
    105 ->  110001
    103 ->   11001
    101 ->    1101
    117 ->  111000
    102 ->  111001
     97 ->   11101
    118 ->    1111
    000101011110001001101111010011111001011100111100110010101101111011011011001100001111010111011001110101100101011110000100000010000010101010110001111111100011010111011100010011001110011101111001010100111111010011100100011110111000101100001010101110011001011

---
+++ huffman-decode.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct pixel{
+	int label;
+	pixel* left, *right;
+
+	pixel(int x):	label(x){
+		left = NULL;
+		right = NULL;
+	}
+
+};
+
+pixel* add(pixel* root, int i, string s, string label){
+
+	if(root == NULL){
+		root = new pixel(-1);
+	}
+
+	if(i == s.length()){
+		cout << s << " " << label << endl; 
+		root->label = stoi(label);
+		return root;
+	}
+
+	if(s[i] == '0'){
+		root->left = add(root->left, i+1, s, label);
+	}
+	else{
+		root->right = add(root->right, i+1, s, label);
+	}
+
+	return root;
+}
+
+void preoder(pixel* root, string s){
+	
+	if(root == NULL)
+		return;
+
+	if(root->label != -1){
+		cout << setfill(' ') << setw(3) << root->label 
+			<< " -> " << setw(7) << s << endl;  
+		return;
+	}
+
+	preoder(root->left, s + '0');
+	preoder(root->right, s + '1');
+
+	return;
+}
+
+int main(){
+	int n;
+	cin>>n;
+	pixel* root = new pixel(-1);
+	for(int i=0; i<n; i++){
+		string a,b,c;
+		cin>>a>>b>>c;
+		add(root, 0, c, a);
+	}
+
+	// for checking
+	preoder(root, "");
+
+	string w;
+	cin >> w;
+
+	pixel* ptr = root;
+
+	vector<int> v;
+
+	for(auto i:w){
+		if(i=='0')
+			ptr = ptr->left;
+		else
+			ptr = ptr->right;
+		if(ptr->label != -1){
+			v.push_back(ptr->label);
+			ptr = root;
+		}
+	}
+
+	for(auto i:v){
+		cout << (char)i ;
+	} cout << endl;
+
+	return 0;
+}
+
+blvbjeanvnkjvnjlearknjnakrnekndcjbhbllivuekrjhggefskvhfbvkjdbsrjgk
